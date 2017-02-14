19ce9fee0add167a2f6d6d04e5489f89b99256c1
Hyunju Yang <hyjyang@kr.ibm.com>
2017-02-14 02:15:16 -0600

    Modify code for the basic chat app

--- app.js
+++ app.js
@@ -21,8 +21,19 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+// http 에서 express app 과 socket.io 동시 사용
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+ 
+io.on('connection', function(socket) {
+    // 채팅 사용자가 P 로 메시지를 보내면 모든 사용자에게 전송
+    socket.on('P', function(msg) {
+        io.emit('P', msg);
+    });
+});
+ 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+http.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
